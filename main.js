686ea32047869b1ba3cdbba55966f189a5e15622
Taylor Jordan <taylornoel418@icloud.com>
2019-04-11 21:04:30 -0600

    Add more query selectors and create function to append task list

--- main.js
+++ main.js
@@ -6,14 +6,20 @@ var taskItemInput = document.querySelector('#task-item-input');
 var makeTaskBtn = document.querySelector('#make-task-list-btn');
 var clearBtn = document.querySelector('#clear-btn');
 var filterBtn = document.querySelector('#filter-btn');
+var toDoListContainer = document.querySelector('.task-list__container');
+var sideBar = document.querySelector('.sidebar__container')
 
 var toDoListArray = JSON.parse(localStorage.getItem('tasksSaved')) || [];
 
 
 /* ------ Event Listeners ------ */
 
+toDoListContainer.addEventListener('click');
+
 searchInput.addEventListener('keyup', );
 searchBtn.addEventListener('click');
+clearBtn.addEventListener('click')
+
 
 
 /* ------ Functions ------ */
@@ -30,6 +36,19 @@ function storeInput(title, id, tasks, urgent) {
 }
 
 function createNewToDoList() {
+	toDoListContainer.innerHTML = 
+	`<div class="task-list">
+			<div class="task-list__header">
+				<p>Task Title</p>
+			</div>
+			<div class="task-list__item-list">
+				<input type="radio"/><p>Don't ever play yourself.</p>
+			</div>
+			<div class="task-list__footer">
+			</div>
+		</div>
+		</div>`
+		+ toDoListContainer.innerHTML;
 
 }
 
